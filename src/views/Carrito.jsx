12a5bab46dc6bb24a27b5a7575238fa76e8d86e8
Augusto Lepe <augusto.lepe@mail.udp.cl>
2023-05-07 23:54:00 -0400

    cart update

--- src/views/Carrito.jsx
+++ src/views/Carrito.jsx
@@ -8,20 +8,20 @@ export default function Carrito() {
 
     return (
         <div className="carrito p-5">
-            <div className="detalles bg-ligth w-75 m-auto p-5">
-                <h5>Detalles del pedido: </h5>
+            <div className="detalles bg-ligth w-50 m-auto p-5">
+                <h3>Detalles del pedido: </h3>
                 <div className="p3 bg-white">
                     {agregar.map((producto, i) => (
                         <div key={i} className="d-flex justify-content-between py-2">
 
                             <div className="d-flex justify-content-between aling-item-center">
-                                <img src={producto.img} width="50" alt="" />
-                                <h6 className="mb-0 text-capitalize p-2">{producto.name}</h6>
+                                <img src={producto.img} width="80" alt="" />
+                                <h5 className="mb-0 text-capitalize p-2">{producto.name}</h5>
                             </div>
-                            <div className="d-flex justify-content-end aling-item-center">
-                                <h6 className="mb-0 p-2 text-success">${(producto.price * producto.count).toLocaleString("es-Cl")}</h6>
+                            <div className="d-flex justify-content-end align-items-center">
+                                <h5 className="mb-0 p-2 text-success">${(producto.price * producto.count).toLocaleString("es-Cl")}</h5>
                                 <Button variant="danger" onClick={() => decrement(i)}>-</Button>
-                                <b className="mx-2">{producto.count}</b>
+                                <h5 className="mb-0 mx-2">{producto.count}</h5>
                                 <Button variant="primary" onClick={() => incrementar(i)}>+</Button>
                             </div>
                         </div>
